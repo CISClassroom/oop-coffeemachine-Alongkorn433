d5ee0726fd9ea5126a012f7a7800d2c5690ffb1c
อลงกรณ์ ประดิษฐวงษ์ <59078983+Alongkorn433@users.noreply.github.com>
2020-03-08 23:18:44 +0700

    coffee 623410037-1

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Coffee coffeeMachine = new Coffee();
+        coffeeMachine.MakeCoffee(6);
+
+    }
+}
